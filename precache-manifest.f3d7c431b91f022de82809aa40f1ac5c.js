bd07744a596ab15f2060f0d35e707e098e174e88
Hong Anh <hong@shopstack.asia>
2019-03-11 14:24:16 +0700

    Updates

---
+++ precache-manifest.f3d7c431b91f022de82809aa40f1ac5c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "038e646f4e4a8aeecde6",
+    "url": "/lasthomework/static/js/runtime~main.038e646f.js"
+  },
+  {
+    "revision": "c4b5eb60dc9504eb28cd",
+    "url": "/lasthomework/static/js/main.c4b5eb60.chunk.js"
+  },
+  {
+    "revision": "54250802618bbe2a76f6",
+    "url": "/lasthomework/static/js/2.54250802.chunk.js"
+  },
+  {
+    "revision": "8bc32083a9bd91eefaf88b35e06b0f63",
+    "url": "/lasthomework/index.html"
+  }
+];
